5ef13be1d43095416a19c89254c3dabbe1344220
Micha Dery <mdery@nvidia.com>
2025-09-18 21:58:34 +0300

    Add multi-field channel support
    Co-authored-by: apriakhin <apriakhin@nvidia.com>

--- src/realm/transfer/channel.h
+++ src/realm/transfer/channel.h
@@ -56,7 +56,7 @@ namespace Realm {
 
   typedef unsigned long long XferDesID;
 
-// clang-format off
+  // clang-format off
 #define REALM_XFERDES_KINDS(__op__) \
   __op__(XFER_NONE) \
   __op__(XFER_DISK_READ) \
@@ -738,6 +738,11 @@ namespace Realm {
     bool has_non_redop_path{false};
 
     virtual bool supports_redop(ReductionOpID redop_id) const;
+
+    virtual bool support_idindexed_fields(Memory src_mem, Memory dst_mem) const
+    {
+      return false;
+    };
 
     // attempt to make progress on the specified xferdes
     virtual long progress_xd(XferDes *xd, long max_nr);
